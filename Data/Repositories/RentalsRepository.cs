f76a5e6785498a4c6523c94fa3dffd57f8f715ca
rrchirinosc <rrchirinosc@gmail.com>
2023-07-03 02:42:34 +0200

    - added methods to complete and retrieve bookings

--- Data/Repositories/RentalsRepository.cs
+++ Data/Repositories/RentalsRepository.cs
@@ -27,5 +27,55 @@ namespace ASolCarRental.Repositories
             return _connection.Query<AvailableCarsDTO>(sql);           
         }
 
+        public IEnumerable<AvailableCarsDTO> GetAvailableCarsByType(short carType)
+        {
+            string sql = string.Format("SELECT [Cars].Id, [Cars].Registration, [Cars].Type, [Cars].Description" +
+                                        " FROM [Cars]" +
+                                        " WHERE [Cars].Available = 1 AND [Cars].type = {0}", carType);
+
+            return _connection.Query<AvailableCarsDTO>(sql);
+        }
+
+        public IEnumerable<AvailableCarsDTO> LoadCarById(int carId)
+        {
+            string sql = string.Format("SELECT [Cars].Id, [Cars].Registration, [Cars].Mileage, [Cars].Description, [Cars].Type" +
+                                        " FROM [Cars]" +
+                                        " WHERE [Cars].Available = 1 AND [Cars].Id = {0}", carId);
+
+            return _connection.Query<AvailableCarsDTO>(sql);
+        }
+
+
+        public int BookCar(BookingDTO Booking)
+        {
+            string sql = "INSERT INTO [Rentals] (CarId, RentalDate, CustomerId, CarMileage, CarType, CarRegistration)" +
+                    " Output Inserted.BookingId" +
+                    " Values (@CarId, @RentalDate, @CustomerId, @CarMileage, @CarType, @CarRegistration)";
+
+            var obj = _connection.QuerySingle(sql, new { Booking.CarId, Booking.RentalDate, Booking.CustomerId, Booking.CarMileage, Booking.CarType, Booking.CarRegistration});
+
+            return obj != null ? obj.BookingId : 0;
+        }
+
+        public int SetCarAsUnAvailable(int carId){
+            string sql = "UPDATE [Cars] SET [Available] = @state" +
+                        " WHERE[Id] = @id";
+
+            return _connection.Execute(sql, new
+            {
+                state = false,
+                id = carId
+            });
+        }
+
+
+        public IEnumerable<BookingDTO> GetBookingData(string bookingId)
+        {
+            string sql = string.Format("SELECT *" +
+                                        " FROM [Rentals]" +
+                                        " WHERE [Rentals].BookingId = {0}", bookingId);
+
+            return _connection.Query<BookingDTO>(sql);
+        }
     }
 }
