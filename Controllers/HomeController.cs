fe35c1a73564962164a36a03bdf20623f0f5852e
rrchirinosc <rrchirinosc@gmail.com>
2023-07-03 02:40:19 +0200

    - added methods to complete booking implementation - started with car returns

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using ASolCarRental.Infrastructure.Options;
 using Microsoft.Extensions.Options;
+using ASolCarRental.Data.DTO;
+
 
 namespace ASolCarRental.Controllers
 {
@@ -30,13 +32,72 @@ namespace ASolCarRental.Controllers
         }
 
 
-        public async Task<IActionResult> Booking()
+        public IActionResult Booking()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetCarList(int carType)
         {
             PreBookingViewModel model;
 
-            model = await PreBookingViewModel.LoadCars(Connection());
-            return View(model);
+            model = await PreBookingViewModel.LoadCarsByType(connection, carType);
+            return Json(model.AvailableCars);
         }
+
+        public async Task<JsonResult> GetCarData(int carId)
+        {
+            PreBookingViewModel model;
+
+            model = await PreBookingViewModel.LoadCarById(connection, carId);
+            return Json(model.AvailableCars);
+        }
+
+
+        public async Task<JsonResult> GetBookingData(string bookingId)
+        {
+            BookingViewModel model;
+
+            model = await BookingViewModel.GetBookingData(connection, bookingId);
+            return Json(model.Booking);
+        }
+
+        [HttpPost]
+        public async Task<int> BookCar(int CarId, int Mileage, DateTime BookingDate, short BookingTime, string CustomerId, string CarRegistration,
+                                        short CarType)
+        {
+            BookingDTO booking = new();
+            int BookingId = 0;
+
+            booking.BookingId = 0;  // will be generated
+            booking.CarId = CarId;
+            booking.RentalDate = BookingDate.AddHours(BookingTime + 8);
+            booking.CarMileage = Mileage;
+            booking.CustomerId = CustomerId;
+            booking.CarRegistration = CarRegistration;
+            booking.CarType = CarType;
+
+            BookingId = await BookingViewModel.BookCar(connection, booking);
+
+            if(BookingId != 0) {
+                // Mark booked car as unavailable
+                //TODO: Handle possible error
+                int result = await SetCarAsUnAvailable(CarId);
+            }
+
+            return BookingId;
+        }
+
+
+        private async Task<int> SetCarAsUnAvailable(int carId)
+        {
+            int result = await BookingViewModel.SetCarAsUnAvailable(connection, carId);
+            return result;
+        }
+
+
+
         public IActionResult BookingReturn()
         {
             return View();
